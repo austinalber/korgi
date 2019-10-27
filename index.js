4090c920fabc50d576599e04e2b675deda9ee7d3
Austin Alber <austinalber@gmail.com>
2019-10-27 16:11:51 -0400

    Added initial backend files

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+// IMPORT MODELS
+require('./models/Product');
+
 // Create Express Application
 const app = express();
 
@@ -11,6 +14,19 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/korgi-project`);
 
 app.use(bodyParser.json());
+
+//IMPORT ROUTES
+require('./routes/productRoute')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+  
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
