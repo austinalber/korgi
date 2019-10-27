d590f0b7457eaa63a65c5a13322a46d320c6d52e
Austin Alber <austinalber@gmail.com>
2019-10-27 16:45:33 -0400

    Added validations to /models/User.js

--- models/User.js
+++ models/User.js
@@ -2,10 +2,28 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const userSchema = new Schema({
-    firstName: String,
+    firstName: {
+        type: String,
+        required: true
+    },
     lastName: String,
-    password: String,
-    email: String,
+    password: {
+        type: String,
+        required: true,
+        allowNull: false,
+        validate: {
+            len: [1, 60]
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        allowNull: false,
+        validate: {
+            len: [1, 40]
+        }
+    },
     zipCode: Number
 })
 
