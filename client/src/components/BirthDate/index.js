4ac39138a0713478bace2d7f7eccd256502a6166
tindoann <tindoann@gamil.com>
2019-11-02 09:37:32 -0400

    fixed signup population

--- client/src/components/BirthDate/index.js
+++ client/src/components/BirthDate/index.js
@@ -50,7 +50,7 @@ const useStyles = makeStyles(theme => ({
 
 export default function CustomizedSelects() {
   const classes = useStyles();
-  const [age, setAge] = React.useState('');
+  const [day, month, year, setAge] = React.useState('');
   const handleChange = event => {
     setAge(event.target.value);
   };
@@ -62,31 +62,69 @@ export default function CustomizedSelects() {
         <Select
           labelId="demo-customized-select-label"
           id="demo-customized-select"
-          value={age}
+          value={month}
           onChange={handleChange}
           input={<BootstrapInput />}
         >
 
-          <MenuItem value="Day">
+          <MenuItem value="month">
             <em></em>
           </MenuItem>
-          <MenuItem value={10}>1</MenuItem>
-          <MenuItem value={20}>2</MenuItem>
-          <MenuItem value={30}>3</MenuItem>
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+          <MenuItem value={4}>4</MenuItem>
+          <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={6}>6</MenuItem>
+          <MenuItem value={7}>7</MenuItem>
+          <MenuItem value={8}>8</MenuItem>
+          <MenuItem value={9}>9</MenuItem>
+          <MenuItem value={10}>10</MenuItem>
+          <MenuItem value={11}>11</MenuItem>
+          <MenuItem value={12}>12</MenuItem>
+          
         </Select>
       </FormControl>
       <FormControl className={classes.margin}>
         <InputLabel htmlFor="demo-customized-select-native">Day</InputLabel>
         <NativeSelect
           id="demo-customized-select-native"
-          value={age}
+          value={day}
           onChange={handleChange}
           input={<BootstrapInput />}
         >
           <option value="" />
-          <option value={10}>Ten</option>
-          <option value={20}>Twenty</option>
-          <option value={30}>Thirty</option>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+          <option value={6}>6</option>
+          <option value={7}>7</option>
+          <option value={8}>8</option>
+          <option value={9}>9</option>
+          <option value={10}>10</option>
+          <option value={11}>11</option>
+          <option value={12}>12</option>
+          <option value={13}>13</option>
+          <option value={14}>14</option>
+          <option value={15}>15</option>
+          <option value={16}>16</option>
+          <option value={17}>17</option>
+          <option value={18}>18</option>
+          <option value={19}>19</option>
+          <option value={20}>20</option>
+          <option value={21}>21</option>
+          <option value={22}>22</option>
+          <option value={23}>23</option>
+          <option value={24}>24</option>
+          <option value={25}>25</option>
+          <option value={26}>26</option>
+          <option value={27}>27</option>
+          <option value={28}>28</option>
+          <option value={29}>29</option>
+          <option value={30}>30</option>
+          <option value={31}>31</option>
         </NativeSelect>
       </FormControl>
 
