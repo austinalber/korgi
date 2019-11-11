ffcb149be3526080a0aefba3c67b2238863ba572
tindoann <tindoann@gamil.com>
2019-11-10 21:20:14 -0500

    profile setup

--- client/src/components/LikeBtn/index.js
+++ client/src/components/LikeBtn/index.js
@@ -1,43 +1,39 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-export default class LikeButton extends Component {
-  state = {
-    likedBefore: false,
-    likes: 1
-  };
+// class LikeBtn extends Component {
+//   state = {
+//     likedBefore: false,
+//     likes: 1
+//   };
 
-  render() {
-    return (
-      <>
-        <h2>Heart Button</h2>
-        <button
-          onClick={() =>
-            this.setState({
-              likes: this.state.likedBefore
-                ? this.state.likes - 1
-                : this.state.likes + 1,
-              likedBefore: !this.state.likedBefore
-            })
-          }
-          className={("like-button", {
-            liked: this.state.likedBefore
-          })}
-        >
-          Like <span className="likes-counter"></span>
-        </button>
+//   const [likedBefore, false] = useState(...)
 
-        <style>{`
-          .like-button {
-            font-size: 1rem;
-            padding: 5px 10px;
-            color: #585858;
-          }
-          .liked {
-            font-weight: bold;
-            color: #1565c0;
-          }
-        `}</style>
-      </>
-    );
-  }
-}
+//   this.setState({
+//     likes: this.state.likedBefore
+//       ? this.state.likes - 1
+//       : this.state.likes + 1,
+//     likedBefore: !this.state.likedBefore
+//   })
+
+//   render() {
+//     return (
+//         <button
+//           onClick={() => this.updateLikes
+//           }
+
+
+//           className={("like-button", {
+//             liked: this.state.likedBefore
+//           })}
+//         >
+//           Like <span className="liked"></span>
+//         </button>
+//       </>
+//     );
+//   }
+// }
+
+// export default LikeBtn;
+
+// // likes: count
+// // likedBefore: boolean
---
+++ client/src/components/Profile/index.js
@@ -0,0 +1,41 @@
+import React from 'react'; 
+  
+const Profile = () => {
+    return (
+        <React.fragment>
+            <div className="userProfile">
+                <h1 className="text-center">User Profile: {props.firstName}</h1>
+                    <div className="profileImg">
+                        <Thumbnail src={props.userImage} alt={'Avatar'} thumbnail/>
+                    </div>
+                    <div>
+                        <h5>Name</h5>
+                        <p>{props.firstName}</p>
+                        <p>{props.lastName}</p>
+                    </div>
+                    <div>
+                        <h5>Email Address</h5>
+                        <p>{props.email}</p>
+                    </div>
+                    <div>
+                        <h5>Date of Birth</h5>
+                        <p>{props.birthMonth + props.birthDay + props.birthYear}</p> 
+                    </div>
+                    <div>
+                        <h5>Zip Code</h5>
+                        <p>{props.zipCode}</p>
+                    </div>
+                    <div>
+                        <h5>Begin Date</h5>
+                        <p>{props.creationDate}</p>
+                    </div>
+              </div>
+
+        <button className="editBtn" onClick={editBtn}>
+            Edit
+        </button> 
+   </React.fragment>
+   )
+}
+
+export default Profile; 
