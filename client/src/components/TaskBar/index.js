73461afdd08c17842e988fcd8e19a1618d5398a3
tindoann <tindoann@gamil.com>
2019-11-12 03:41:01 -0500

    item passing through - testing

--- client/src/components/TaskBar/index.js
+++ client/src/components/TaskBar/index.js
@@ -8,7 +8,7 @@ const TaskBar = props => {
   return (
       <nav className="navbar navbar-expand-sm bg-light justify-content-center ">
          <div className="navbar-nav">
-            <h1 id='task'>{props.tasks}</h1>
+           <h1 style={{textAlign: "center"}}>Hello</h1> 
             {/* <div>
                <div>{props.tasks.task.map((item) => <p>{item}</p>)}</div>
             </div> */}
