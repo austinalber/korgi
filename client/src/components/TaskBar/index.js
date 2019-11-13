c253a4cb740f2a99d7767cd814e92fb85937bebe
Austin Alber <austinalber@gmail.com>
2019-11-13 15:53:50 -0500

    Deleted files

--- client/src/components/TaskBar/index.js
+++ client/src/components/TaskBar/index.js
@@ -6,15 +6,18 @@ const TaskBar = props => {
   // let taskList = this.props.tasks;
   
   return (
-      <nav className="navbar navbar-expand-sm bg-light justify-content-center">
-         <div className="navbar-nav">
-            <h1 id='task'>{props.tasks}</h1>
-            {/* <div>
-               <div>{props.tasks.task.map((item) => <p>{item}</p>)}</div>
-            </div> */}
-        </div>       
-      </nav>
-  )
+     <div>
+        hello
+     </div>
+      // <nav className="navbar navbar-expand-sm bg-light justify-content-center">
+      //    <div className="navbar-nav">
+      //       <h1 id='task'>{props.tasks}</h1>
+      //       {/* <div>
+      //          <div>{props.tasks.task.map((item) => <p>{item}</p>)}</div>
+      //       </div> */}
+      //   </div>       
+      // </nav>
+  );
 }
 
 export default TaskBar; 
