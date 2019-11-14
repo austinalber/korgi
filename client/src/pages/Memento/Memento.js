b6fb27b7d8d8ae64eccd05eaaa52886aea00fef4
Austin Alber <austinalber@gmail.com>
2019-11-14 01:27:34 -0500

    Added to memento styling and possible increaded data for postImage

--- client/src/pages/Memento/Memento.js
+++ client/src/pages/Memento/Memento.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
+// import ImageUploader from "react-images-upload";
+import PropTypes from "prop-types";
 import { postCard } from "../../actions/authActions";
 import "./style.css";
 
@@ -56,50 +57,73 @@ class Memento extends Component {
     reader.readAsDataURL(file);
   };
 
+  // onDrop(value) {
+  //   // this.setState({
+  //   //     file: value
+  //   // });
+
+  //   let reader = new FileReader();
+  //   reader.readAsDataURL(value);
+  //   reader.onloadend = () => {
+  //       this.setState({
+  //           file: reader.result
+  //       });
+  //   };
+  // }
+
   render() {
     const { errors } = this.state;
 
     return (
       <div className="post-outer">
-          <h4 className="welcome-text">Share your story !</h4>
-          <div className  ='post-div'>
-            <label className="custom-file-upload fas">
-              <div className="img-wrap img-upload">
-                <img alt="" src={this.state.picture}/>
-              </div>
-              <input type="file" id="postImage" value={this.state.postImage} onChange={this.photoUpload}/>
-            </label>
-          <div className="text-div">
-              <input type="file" accept="image/x-png,image/jpeg" title=" "/>
-              {errors.postImage && (
-                  <p className="error-text">{errors.postImage}</p>
-              )}
-              <input
-                  value={this.state.location}
-                  className="input-style"
-                  type="text"
-                  placeholder="Location"
-                  name="location"
-                  id="location"
-                  onChange={this.onChange}
-                  error={errors.location}
-                  style={{width: "100%"}}
-              />
-              {errors.location && (
-              <p className="error-text">{errors.location}</p>
-              )}
-              <textarea
-                  value={this.state.caption}
-                  className="input-style"
-                  placeholder="Caption"
-                  id="caption"
-                  onChange={this.onChange}
-                  error={errors.caption}
-              />
-              {errors.caption && (
-              <p className="error-text">{errors.caption}</p>
-              )}
-              <button className="post-card" onClick={this.onSubmit}>Post Card!</button>
+        <h4 className="welcome-text">Share your story !</h4>
+        <div className  ='post-div'>
+        {/* <ImageUploader
+          withIcon={true}
+          buttonText='Choose images'
+          onChange={this.onDrop}
+          imgExtension={['.jpg', '.png']}
+          maxFileSize={1048576} // in bytes
+          label="Max file size: 1MB, accepted: jpb | png"
+          withPreview={true}
+        /> */}
+        <label className="custom-file-upload fas">
+          <div className="img-wrap img-upload">
+            <img alt="" src={this.state.picture}/>
+          </div>
+          <input type="file" id="postImage" value={this.state.postImage} onChange={this.photoUpload}/>
+        </label>
+        <div className="text-div">
+            <input type="file" accept="image/x-png,image/jpeg" title=" "/>
+            {errors.postImage && (
+                <p className="error-text">{errors.postImage}</p>
+            )}
+            <input
+                value={this.state.location}
+                className="input-style"
+                type="text"
+                placeholder="Location"
+                name="location"
+                id="location"
+                onChange={this.onChange}
+                error={errors.location}
+                style={{width: "100%"}}
+            />
+            {errors.location && (
+            <p className="error-text">{errors.location}</p>
+            )}
+            <textarea
+                value={this.state.caption}
+                className="input-style"
+                placeholder="Caption"
+                id="caption"
+                onChange={this.onChange}
+                error={errors.caption}
+            />
+            {errors.caption && (
+            <p className="error-text">{errors.caption}</p>
+            )}
+            <button className="post-card" onClick={this.onSubmit}>Post Card!</button>
           </div>
         </div>
       </div>
