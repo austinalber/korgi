21d4da7525957f3fc48705759dcf950505598a92
Austin Alber <austinalber@gmail.com>
2019-11-14 21:05:52 -0500

    Fuck 2

--- server.js
+++ server.js
@@ -43,11 +43,11 @@ app.use("/api/cards", cards);
 app.use(require("cors"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
 }
 
 app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile('client/build/index.html');
 });
 
 const PORT = process.env.PORT || 5000;
